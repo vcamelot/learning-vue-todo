fbf1becd4bd41bbd5b0bf92a8f4e2fccbd88e8b3
Dmitry Vinogradov <vinogradov.dmitry@gmail.com>
2020-11-22 22:07:43 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4 class="bg-primary text-white text-center p-2">
+      {{ name }}'s To Do List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Tasks</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{ t.action }}</div>
+        <div class="col-2">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+          {{ t.done }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      name: "Dmitry",
+      tasks: [
+        { action: "Buy flowers", done: false },
+        { action: "Get shoes", done: false },
+        { action: "Collect tickets", done: true },
+        { action: "Call Joe", done: false },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
