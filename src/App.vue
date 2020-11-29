c9c6f20dd52b5925ca73e873ce72e489bcf43fa6
Dmitry Vinogradov <vinogradov.dmitry@gmail.com>
2020-11-29 23:04:06 +0200

    Special message shown if there are no pending tasks

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,29 @@
       {{ name }}'s To Do List
     </h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Tasks</div>
-        <div class="col-2 font-weight-bold">Done</div>
-      </div>
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{ t.action }}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="t.done" class="form-check-input" @change="saveTodos" />
-          {{ t.done }}
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nothing to do. Hurrah!</b>
         </div>
       </div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Tasks</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{ t.action }}</div>
+          <div class="col-2 text-center">
+            <input
+              type="checkbox"
+              v-model="t.done"
+              class="form-check-input"
+              @change="saveTodos"
+            />
+            {{ t.done }}
+          </div>
+        </div>
+      </template>
       <div class="row py-2">
         <div class="col">
           <input v-model="newItemText" class="form-control" />
@@ -74,7 +86,7 @@ export default {
     saveTodos() {
       localStorage.setItem("todos", JSON.stringify(this.tasks));
       console.log(JSON.stringify(this.tasks));
-    }
+    },
   },
   created() {
     let data = localStorage.getItem("todos");
