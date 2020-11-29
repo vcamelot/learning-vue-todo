b9578500e778fcaccd868067842c69e33cb518e2
Dmitry Vinogradov <vinogradov.dmitry@gmail.com>
2020-11-29 22:43:06 +0200

    Added completed tasks filter and Add new task feature

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,31 @@
         <div class="col font-weight-bold">Tasks</div>
         <div class="col-2 font-weight-bold">Done</div>
       </div>
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
         <div class="col">{{ t.action }}</div>
-        <div class="col-2">
+        <div class="col-2 text-center">
           <input type="checkbox" v-model="t.done" class="form-check-input" />
           {{ t.done }}
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input
+            type="checkbox"
+            v-model="hideCompleted"
+            class="form-check-input"
+          />
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
         </div>
       </div>
     </div>
@@ -31,7 +51,25 @@ export default {
         { action: "Collect tickets", done: true },
         { action: "Call Joe", done: false },
       ],
+      hideCompleted: true,
+      newItemText: "",
     };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted
+        ? this.tasks.filter((t) => !t.done)
+        : this.tasks;
+    },
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false,
+      });
+      this.newItemText = "";
+    },
   },
 };
 </script>
